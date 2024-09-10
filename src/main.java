030d9eeeae5d86363cbbf91bae7f0da82bc14304
HenriqueJoanoni <henrique.joanoni@gmail.com>
2024-09-10 15:50:38 +0100

    refactor(Main): Refactoring some functions

--- src/main.java
+++ src/main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class main {
@@ -6,46 +5,49 @@ public class main {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
-            Integer option, firstInput, secondInput;
+            int option;
+            long firstInput, secondInput;
 
             menu();
             option = sc.nextInt();
 
-           switch (option) {
-               case 1:
-                   System.out.println("Enter first number");
-                   firstInput = sc.nextInt();
-                   System.out.println("Enter second number");
-                   secondInput = sc.nextInt();
-                   sum(firstInput, secondInput);
-                   break;
-               case 2:
-                   System.out.println("Enter first number");
-                   firstInput = sc.nextInt();
-                   System.out.println("Enter second number");
-                   secondInput = sc.nextInt();
-                   subtract(firstInput, secondInput);
-                   break;
-               case 3:
-                   System.out.println("Enter first number");
-                   firstInput = sc.nextInt();
-                   System.out.println("Enter second number");
-                   secondInput = sc.nextInt();
-                   multiply(firstInput, secondInput);
-                   break;
-               case 4:
-                   System.out.println("Enter first number");
-               firstInput = sc.nextInt();
-                   System.out.println("Enter second number");
-               secondInput = sc.nextInt();
-               divide(firstInput, secondInput);
-               break;
-           }
+            if (option < 1 || option > 4) {
+                System.out.println(">>> Option is out of bounds!");
+            }
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter first number");
+                    firstInput = sc.nextInt();
+                    System.out.println("Enter second number");
+                    secondInput = sc.nextInt();
+                    System.out.println("The result is: " + sum(firstInput, secondInput));
+                    break;
+                case 2:
+                    System.out.println("Enter first number");
+                    firstInput = sc.nextInt();
+                    System.out.println("Enter second number");
+                    secondInput = sc.nextInt();
+                    System.out.println("The result is: " + subtract(firstInput, secondInput));
+                    break;
+                case 3:
+                    System.out.println("Enter first number");
+                    firstInput = sc.nextInt();
+                    System.out.println("Enter second number");
+                    secondInput = sc.nextInt();
+                    System.out.println("The result is: " + multiply(firstInput, secondInput));
+                    break;
+                case 4:
+                    System.out.println("Enter first number");
+                    firstInput = sc.nextInt();
+                    System.out.println("Enter second number");
+                    secondInput = sc.nextInt();
+                    System.out.println("The result is: " + divide(firstInput, secondInput));
+                    break;
+            }
         } catch (Exception e) {
-            System.out.println("Something went wrong");
+            System.out.println(">>> Something went wrong");
         }
-
-
     }
 
     public static void menu() {
@@ -57,43 +59,43 @@ public class main {
 
     }
 
-    public static int sum(int a, int b) {
-        int result = 0;
+    public static int sum(long a, long b) {
+        long result = 0;
         try {
-            result= a + b;
+            result = a + b;
         } catch (Exception e) {
-            System.out.println("Function sum error");
+            System.out.println(">>> Something went wrong (sum)");
         }
-        return result;
+        return (int) result;
     }
 
-    public static int subtract(int a, int b) {
-        int result = 0;
+    public static int subtract(long a, long b) {
+        long result = 0;
         try {
-            result= a - b;
+            result = a - b;
         } catch (Exception e) {
-            System.out.println("Something went wrong (subtract)");
+            System.out.println(">>> Something went wrong (subtract)");
         }
-        return result;
+        return (int) result;
     }
 
-    public static int multiply(int a, int b) {
-        int result = 0;
+    public static int multiply(long a, long b) {
+        long result = 0;
         try {
-            result= a * b;
+            result = a * b;
         } catch (Exception e) {
-            System.out.println("Something went wrong (multiply)");
+            System.out.println(">>> Something went wrong (multiply)");
         }
-        return result;
+        return (int) result;
     }
 
-    public static int divide(int a, int b) {
-        int result = 0;
+    public static int divide(long a, long b) {
+        long result = 0;
         try {
-            result= a / b;
+            result = a / b;
         } catch (Exception e) {
-            System.out.println("Something went wrong (divide)");
+            System.out.println(">>> Something went wrong (divide)");
         }
-        return result;
+        return (int) result;
     }
 }
