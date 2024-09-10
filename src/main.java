9975e9ab8d77b8a9d8c2dcfff147c67df5eaf82e
HenriqueJoanoni <henrique.joanoni@gmail.com>
2024-09-10 14:33:44 +0100

    build(Project): Initial Functions

--- src/main.java
+++ src/main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer firstInput, secondInput;
 
+
+    }
+
+    public static void menu() {
+
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
